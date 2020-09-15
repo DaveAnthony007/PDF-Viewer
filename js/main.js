cf9e695a5eaf45396b639409bfa367b342a26b62
unknown <bcamaster@P090MA121.intra.bca.co.id>
2020-09-15 14:11:58 +0700

    Add default pdf viewer and custom pdf viewer

---
+++ js/main.js
@@ -0,0 +1,99 @@
+const url = '../docs/grimm10.pdf'
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null
+
+const scale = 1.25,
+    canvas = document.querySelector('#pdf-render'),
+    ctx = canvas.getContext('2d')
+
+// Render the page
+const renderPage = num => {
+    pageIsRendering = true
+
+    
+    // Get page
+    pdfDoc.getPage(parseInt(num)).then(page => {
+        // Set scale
+        const viewport = page.getViewport({scale})
+        canvas.height = viewport.height
+        canvas.width = viewport.width
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false
+            if(pageNumIsPending !== null) {
+                renderPage(pageNumIsPending)
+                pageNumIsPending = null
+            }
+        })
+
+        // Output current page
+        document.querySelector('#page-num').value = num
+    }, error => {
+        console.log('something', num)
+        console.log('error', error)
+    })
+}
+
+// Check for pages rendering
+const queueRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsRendering = num
+    } else {
+        renderPage(num)
+    }
+}
+
+
+// Show Prev Page
+const showPrevPage = () => {
+    if(pageNum <= 1) {
+        return
+    }
+    pageNum--
+    queueRenderPage(pageNum)
+}
+
+// Show Next Page
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages) {
+        return
+    }
+    pageNum++
+    queueRenderPage(pageNum)
+}
+
+
+// Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_
+    
+    document.querySelector('#page-count').textContent = pdfDoc.numPages
+    renderPage(pageNum)
+})
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage)
+document.querySelector('#next-page').addEventListener('click', showNextPage)
+
+// Oninput Events
+const jumpToPage = () => {
+    let num = document.querySelector('#page-num').value
+    num = (num == "")?pageNum:num
+    console.log('page num', num)
+    // renderPage(num)
+
+    //
+    if(pageNum > pdfDoc.numPages || pageNum < 1) {
+        return
+    }
+    pageNum = num
+    queueRenderPage(pageNum)
+}
